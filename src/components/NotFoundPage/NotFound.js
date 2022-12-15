026ec5fca8b33abc03edc5ba3c617d15a94ef0ea
Olga GSF <tmpl-toest@yandex.ru>
2022-12-15 17:10:35 +0300

    Fix adaptive2

--- src/components/NotFoundPage/NotFound.js
+++ src/components/NotFoundPage/NotFound.js
@@ -4,7 +4,7 @@ function NotFound() {
       <section className="not-found">
         <p className="not-found__title">404</p>
         <p className="not-found__subtitle">Страница не найдена</p>
-        <button className="not-found__button" href="#">Найти</button>
+        <button className="not-found__button" href="#">Назад</button>
       </section>
     </>
   )
